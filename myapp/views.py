0b4d141e16ec26ea81d5badc0784ed440e14a43f
jumasheff <jumasheff@gmail.com>
2018-05-01 19:25:57 +0600

    Use get_object_or_404, fix duplicate creation

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect, get_object_or_404 
 from .models import Car
 from .forms import CarModelForm
 
@@ -14,15 +14,16 @@ def index(request):
     return render(request, 'index.html', context)
 
 def car_details(request, pk):
-    carss = Car.objects.get(pk=pk)
+    carss = get_object_or_404(Car, pk=pk)
     context = {'c': carss}
     return render(request, 'car_details.html', context)
 
 def car_edit(request, pk):
-    car = Car.objects.get(pk=pk)
+    car = get_object_or_404(Car, pk=pk)
     form = CarModelForm(instance=car)
     if request.method == 'POST':
-        form = CarModelForm(request.POST)
+        # Сохраняем изменения в экземпляре car (т.е. не создаем новую запись)
+        form = CarModelForm(request.POST, instance=car)
         if form.is_valid():
             car = form.save()
             return redirect('myapp:car_details', pk=car.pk)
@@ -30,7 +31,7 @@ def car_edit(request, pk):
     return render(request, 'car_edit.html', context)
 
 def car_delete(request, pk):
-    car = Car.objects.get(pk=pk)
+    car = get_object_or_404(Car, pk=pk)
     if request.method == 'POST':
         car.delete()
         return redirect('myapp:index')
