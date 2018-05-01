d83f48018895029c689a7347cf82c5809a4f8794
jumasheff <jumasheff@gmail.com>
2018-05-01 20:38:09 +0600

    Add new fields

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from .models import Car, Garage
 
+admin.site.register(Car)
+admin.site.register(Garage)
 
 
--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+class Garage(models.Model):
+    name = models.CharField(max_length=50)
+    capacity = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return "{} {}".format(self.name, self.capacity)
+
 class Car(models.Model):
     name = models.CharField(max_length=100)
     year = models.PositiveSmallIntegerField()
+    garage = models.ForeignKey(
+        Garage, 
+        on_delete=models.CASCADE, 
+        related_name='cars',
+        related_query_name='car'
+    )
     is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
-
-    
