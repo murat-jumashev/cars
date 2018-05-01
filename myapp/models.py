92e6d14962d48870ac112f3165b355bd8e8618a9
jumasheff <jumasheff@gmail.com>
2018-05-01 19:41:19 +0600

    Add new field

--- myapp/models.py
+++ myapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Car(models.Model):
     name = models.CharField(max_length=100)
     year = models.PositiveSmallIntegerField()
+    is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
